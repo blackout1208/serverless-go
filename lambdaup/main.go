e853b3b09f606894abbfeb65930355dcb3aed3e5
carlosduarte12 <carlosduarte.work@gmail.com>
2021-01-30 17:47:54 +0100

    removed debugging logs

--- lambdaup/main.go
+++ lambdaup/main.go
@@ -27,34 +27,29 @@ func NewLProject(fname string) (*LProject, error) {
 	// Read json file
 	data, err := ioutil.ReadFile(fname)
 	if err != nil {
-		log.Println(201)
 		return nil, err
 	}
 
 	var res LProject
 	if err = json.Unmarshal(data, &res); err != nil {
-		log.Println(3012, string(data))
 		return nil, err
 	}
 
 	// the directory where the json file is, is the root folder of the project
 	res.path = path.Dir(fname)
 	if strings.HasPrefix(res.Role, "arn:") {
-		log.Println(2012)
 		//if reading from json file already has role
 		return &res, nil
 	}
 	// get the role map
 	rmp, err := RoleMap()
 	if err != nil {
-		log.Println(2013)
 		return nil, err
 	}
 
 	// extract the role map
 	nRole, ok := rmp[res.Role]
 	if !ok {
-		log.Println(2014)
 		return &res, errors.New("Role not found: " + res.Role)
 	}
 	res.Role = nRole
@@ -74,7 +69,6 @@ func (lp *LProject) UploadLambda(name string) error {
 	// build function
 	fmt.Println("Building : " + fpath + ".go")
 	if _, err := run("go", "build", "-o", fpath, fpath+".go"); err != nil {
-		log.Println(2020, fpath)
 		return err
 	}
 
@@ -82,7 +76,6 @@ func (lp *LProject) UploadLambda(name string) error {
 	// -j means, ignore the folder path in the name and set the zip file only with the name of the original file
 	fmt.Println("Zipping to " + fpath + ".zip")
 	if _, err := run("zip", "-j", fpath+".zip", fpath); err != nil {
-		log.Println(2021)
 		return err
 	}
 
@@ -96,14 +89,12 @@ func (lp *LProject) UploadLambda(name string) error {
 	// read stdout - read the info "answered" byt the s3 upload
 	upOut, err := upcmd.StdoutPipe()
 	if err != nil {
-		log.Println(2022)
 		return err
 	}
 
 	// here we start the execution of s3 command
 	fmt.Println("Starting Upload of " + lamname)
 	if err := upcmd.Start(); err != nil {
-		log.Println(2023)
 		return err
 	}
 
@@ -111,13 +102,11 @@ func (lp *LProject) UploadLambda(name string) error {
 	io.Copy(os.Stdout, upOut)
 	// wait for the copy command to finish
 	if err := upcmd.Wait(); err != nil {
-		log.Println(2024)
 		return err
 	}
 
 	fl, err := NewFunctionList()
 	if err != nil {
-		log.Println(2025)
 		return err
 	}
 
@@ -126,7 +115,6 @@ func (lp *LProject) UploadLambda(name string) error {
 		fmt.Println("Updating Function " + lamname)
 		resp, err := run("aws", "lambda", "update-function-code", "--function-name", lamname, "--s3-bucket", lp.Bucket, "--s3-key", lamname+".zip")
 		if err != nil {
-			log.Println(2026)
 			return err
 		}
 
@@ -152,11 +140,11 @@ func main() {
 
 	proj, err := NewLProject(*confloc)
 	if err != nil {
-		log.Fatal(11, err)
+		log.Fatal(err)
 	}
 
 	err = proj.UploadLambda(*lname)
 	if err != nil {
-		log.Fatal(22, err)
+		log.Fatal(err)
 	}
 }
